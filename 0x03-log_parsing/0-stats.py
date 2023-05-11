ed0f6e1182a99a7636440d4fc72209057dc15e47
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-05-11 22:26:27 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python3
-'''A script for parsing HTTP request logs.
-'''
+#!/usr/bin/env python3
+
 import sys
 from collections import defaultdict
 
-# Initialize variables
 total_file_size = 0
 status_code_counts = defaultdict(int)
 line_count = 0
 
 try:
     for line in sys.stdin:
-        # Parse line
         try:
             parts = line.split()
             ip_address = parts[0]
@@ -20,23 +17,19 @@ try:
             status_code = int(parts[6])
             file_size = int(parts[7])
         except:
-            # Skip line if it doesn't match expected format
+            # If the line doesn't match the expected format, skip it
             continue
         
-        # Update metrics
         total_file_size += file_size
         status_code_counts[status_code] += 1
         line_count += 1
         
-        # Print statistics every 10 lines
         if line_count % 10 == 0:
-            print(f"Total file size: {total_file_size}")
+            print(f"File size: {total_file_size}")
             for code in sorted(status_code_counts.keys()):
                 print(f"{code}: {status_code_counts[code]}")
         
 except KeyboardInterrupt:
-    # Print final statistics on keyboard interrupt
-    print(f"Total file size: {total_file_size}")
+    print(f"File size: {total_file_size}")
     for code in sorted(status_code_counts.keys()):
         print(f"{code}: {status_code_counts[code]}")
-
