942109ed17a2363d0a89728753718f46885cea3d
Geoffy1 <geojogu@gmail.com>
2023-05-11 13:04:23 -0700

    my logs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,7 +9,6 @@ status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
 
 total_size = 0
 count = 0  # keep count of the number lines counted
-
 try:
     for line in sys.stdin:
         line_list = line.split(" ")
@@ -18,20 +17,16 @@ try:
             status_code = line_list[-2]
             file_size = int(line_list[-1])
 
-
             if status_code in status_codes_dict.keys():
                 status_codes_dict[status_code] += 1
 
-
             total_size += file_size
-
 
             count += 1
 
         if count == 10:
             count = 0  # reset count
             print('File size: {}'.format(total_size))
-
 
             for key, value in sorted(status_codes_dict.items()):
                 if value != 0:
